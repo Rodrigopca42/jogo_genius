1fd631e606754560e07f31259f8a229104fc403b
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-02-06 19:25:16 -0300

    readme
    falha no java script

--- javascript/folha.js
+++ javascript/folha.js
@@ -98,10 +98,10 @@ let playGame = () =>{
     nextLevel();
 }
 
-green.onclick = () => click(0);
-red.onclick = () => click(1);
-yellow.onclick = () => click(2);
-blue.onclick = () => click(3);
+green.onclick  (() => click(0));
+red.onclick (() => click(1));
+yellow.onclick (() => click(2));
+blue.onclick (() => click(3));
   
 
 playGame();
